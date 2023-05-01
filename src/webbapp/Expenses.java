84d3c4e1b49db2e8d89194cc0fdcdc1488adafb3
erdendemirkol <123850988+erdendemirkol@users.noreply.github.com>
2023-05-01 08:38:28 -0700

    Added activity expenses

--- src/webbapp/Expenses.java
+++ src/webbapp/Expenses.java
@@ -1,9 +1,9 @@
 package webbapp;
 
 public class Expenses {
-    // added sme expenses about sales activities
+    // added sme expenses about sales activities page
 
-
+// added personal expenses page
 
 
 }
